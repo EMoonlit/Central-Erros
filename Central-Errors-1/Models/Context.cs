51b3e70c5626d8a88dd5eaf61d14d77d63e27014
unknown <emoonlit@gmail.com>
2021-07-17 19:52:44 -0300

    Initial Configure EntityFramework

---
+++ Central-Errors-1/Models/Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Central_Errors_1.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<User> Users { get; set; } 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"Server=(localdb)\mssqllocaldb=CentralErros;Integrated Security=True");
+        }
+    }
+}
